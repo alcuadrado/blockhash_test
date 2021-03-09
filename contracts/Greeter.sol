31bdf316494384c7eb2e32cdbba4ab918c614d33
Jtremback <hi@jehan.email>
2021-03-09 12:02:57 -0800

    first commit

---
+++ contracts/Greeter.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.7.0;
+
+import "hardhat/console.sol";
+
+contract Greeter {
+    string greeting;
+
+    constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:", _greeting);
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        console.log("BLOCKHASH");
+        console.logBytes32(blockhash(block.number));
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
+}
